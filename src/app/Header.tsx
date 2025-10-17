bde341e921683a0e16c8020c1ce00560d8aa5b9c
Marcos Max Forosteski da Silva <marcos.silva@neomind.com.br>
2025-10-17 11:17:39 -0300

    udpate: Adaptado hero section para mais viewports

--- src/app/Header.tsx
+++ src/app/Header.tsx
@@ -7,23 +7,23 @@ import CtaButton from "@/components/CtaButton";
 
 export default function Header() {
     return (
-        <div className="relative h-[93vh]">
+        <div className="relative h-[93vh] sm:h-[91vh] md:h-[91vh] lg:h-[91vh] xl:h-[92vh] 2xl:h-[95vh]">
             <HeaderBackground />
             <div className="absolute inset-0 z-0 bg-blue-700 opacity-50"></div>
 
             <div className="relative z-10 flex flex-col justify-between h-full">
-                <div className="flex justify-center pt-[5%]">  
+                <div className="flex justify-center pt-[20px] xl:pt-[40px] 2xl:pt-[50px]">  
                     <Image
                         src={'/logo-completa.png'}
                         alt="Logo Lincey Lab completa"
-                        priority
-                        fetchPriority="high"
                         width={500}
                         height={500}
-                        style={{borderRadius : 100 + "px"}}
+                        className="rounded-4xl w-[280px] h-[280px] sm:w-[300px] sm:h-[300px] md:w-[270px] md:h-[270px] lg:w-[300px] lg:h-[300px] xl:w-[350px] xl:h-[350px] 2xl:w-[350px] 2xl:h-[350px] "
+                        priority
+                        fetchPriority="high"
                     />
                 </div>
-                <div className="flex items-center justify-center flex-1">
+                <div className="flex items-center justify-center flex-1 text-lg sm:text-lg md:text-lg lg:text-xl xl:text-2xl 2xl:text-3xl">
                     <Typer />
                 </div>
 
